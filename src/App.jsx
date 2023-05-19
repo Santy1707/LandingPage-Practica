7b040a6f8a399acf8838607ec2c2367f6f605536
Santiago <98236588+Santy1707@users.noreply.github.com>
2023-05-18 22:32:02 -0500

    Proyecto terminado

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import About from './components/About'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import Home from './components/Home'
+import Testimonial from './components/Testimonials'
+import Work from './components/Work'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <Home/>
+        <About/>
+        <Work/>
+        <Testimonial/>
+        <Contact/>
+        <Footer/>
+      </div>
+    </>
+  )
+}
+
+export default App
